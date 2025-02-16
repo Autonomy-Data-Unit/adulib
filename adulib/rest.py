0e19650541fc43efb58caba0228685aedfe9d140
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-02-16 20:53:58 +0000

    overhauled AsyncAPIHandler

--- adulib/rest.py
+++ adulib/rest.py
@@ -133,26 +133,40 @@ def delete(endpoint, headers=None):
 
 # %% ../nbs/src/rest.ipynb 16
 class AsyncAPIHandler:
+    GET="get"
+    PUT="put"
+    POST="post"
+    DELETE="delete"
+    
     def __init__(self,
                  base_url=None,
                  default_params=None,
                  default_headers=None,
                  rate_limit=None,
                  use_cache=True,
-                 cache_dir=None):
+                 cache_dir=None,
+                 call_quota=None):
         """
-        :param base_url: The base URL of the API.
-        :param default_params: A dictionary of default query parameters.
-        :param default_headers: A dictionary of default headers.
-        :param rate_limit: The rate (calls per second) limit for the API.
-        :param use_cache: Whether to use caching for the API.
-        :param cache_dir: The directory to store the cache.
+        A handler for making asynchronous API calls with support for caching, rate limiting, and default parameters.
+
+        :param base_url: The base URL of the API. This will be prepended to all endpoint calls.
+        :param default_params: A dictionary of default query parameters to be included in every request.
+        :param default_headers: A dictionary of default headers to be included in every request.
+        :param rate_limit: The rate limit for API calls, specified as the number of calls per second.
+        :param use_cache: A boolean indicating whether to enable caching of API responses.
+        :param cache_dir: The directory where cached responses will be stored. If None, a temporary directory will be created.
+        :param call_quota: An optional limit on the number of API calls that can be made. If None, there is no limit.
+
+        This class provides methods for making GET, POST, PUT, and DELETE requests asynchronously, while managing
+        caching and rate limiting. It also allows checking and clearing the cache for specific API calls.
         """
         self.base_url = base_url
         self.default_params = default_params or {}
         self.default_headers = default_headers or {}
         self.use_cache = use_cache
         self.cache_dir = cache_dir
+        self.call_quota = call_quota
+        self.call_counter = 0
         
         if use_cache:
             if self.cache_dir is None: self.cache_dir = tempfile.mkdtemp()
@@ -165,6 +179,15 @@ class AsyncAPIHandler:
         else:
             self._rate_limiter = None
         
+    @property
+    def remaining_call_quota(self):
+        if self.call_quota is None:
+            return None
+        return self.call_quota - self.call_counter
+        
+    def reset_quota(self):
+        self.call_counter = 0
+        
     def __get_defaults(self, method, endpoint, params, headers):
         endpoint = urljoin(self.base_url, endpoint) if endpoint else self.base_url
         params = params or {}
@@ -174,29 +197,56 @@ class AsyncAPIHandler:
         cache_key = f"{method}:{endpoint}:{params}:{headers}"
         return endpoint, params, headers, cache_key
     
-    async def __load_cache_or_make_call(self, func, args, cache_key):
-        if self.use_cache and cache_key in self._cache:
+    async def __load_cache_or_make_call(self, func, args, only_use_cache, cache_key):
+        if only_use_cache or (self.use_cache and cache_key in self._cache):
             return self._cache[cache_key]
         else:
+            if self.call_quota is not None and self.remaining_call_quota <= 0:
+                raise RuntimeError("API call quota has been exceeded.")
+            self.call_counter += 1
             if self._rate_limiter: await self._rate_limiter.wait()
             result = await func(*args)
             if self.use_cache: self._cache[cache_key] = result
         return result
     
-    async def get(self, endpoint=None, params=None, headers=None, **param_kwargs):
+    async def call(self, method, endpoint=None, params=None, data=None, headers=None, only_use_cache=False, **param_kwargs):
+        """
+        Make a request to the API.
+
+        :param method: The HTTP method to use (e.g., "get", "put", "post", "delete").
+        :param endpoint: The API endpoint to request.
+        :param params: A dictionary of query parameters for the request.
+        """
         params = params or {}
         params = {**params, **param_kwargs}
-        endpoint, params, headers, cache_key = self.__get_defaults("get", endpoint, params, headers)
-        return await self.__load_cache_or_make_call(async_get, (endpoint, params, headers), cache_key)
+        endpoint, params, headers, cache_key = self.__get_defaults(method, endpoint, params, headers)
+        if method == AsyncAPIHandler.GET: func = async_get
+        elif method == AsyncAPIHandler.PUT: func = async_put
+        elif method == AsyncAPIHandler.POST: func = async_post
+        elif method == AsyncAPIHandler.DELETE: func = async_delete
+        else: raise ValueError(f"Invalid method: {method}")
+        return await self.__load_cache_or_make_call(func, (endpoint, params, headers), only_use_cache, cache_key)
     
-    async def put(self, endpoint=None, data=None, headers=None):
-        endpoint, _, headers, cache_key = self.__get_defaults("put", endpoint, None, headers)
-        return await self.__load_cache_or_make_call(async_put, (endpoint, data, headers), cache_key)
+    async def get(self, endpoint=None, params=None, headers=None, only_use_cache=False, **param_kwargs):
+        return await self.call(self.GET, endpoint, params, headers, only_use_cache, **param_kwargs)
     
-    async def post(self, endpoint=None, data=None, headers=None):
-        endpoint, _, headers, cache_key = self.__get_defaults("post", endpoint, None, headers)
-        return await self.__load_cache_or_make_call(async_post, (endpoint, data, headers), cache_key)
+    async def put(self, endpoint=None, data=None, only_use_cache=False, headers=None):
+        return await self.call(self.PUT, endpoint, data, headers, only_use_cache)
     
-    async def delete(self, endpoint=None, headers=None):
-        endpoint, _, headers, cache_key = self.__get_defaults("delete", endpoint, None, headers)
-        return await self.__load_cache_or_make_call(async_delete, (endpoint, headers), cache_key)
+    async def post(self, endpoint=None, data=None, only_use_cache=False, headers=None):
+        return await self.call(self.POST, endpoint, data, headers, only_use_cache)
+    
+    async def delete(self, endpoint=None, only_use_cache=False, headers=None):
+        return await self.call(self.DELETE, endpoint, headers, only_use_cache)
+    
+    def check_cache(self, method, endpoint=None, params=None, headers=None, **param_kwargs):
+        params = params or {}
+        params = {**params, **param_kwargs}
+        _, _, _, cache_key = self.__get_defaults(method, endpoint, params, headers)
+        return cache_key in self._cache
+    
+    def clear_cache_key(self, method, endpoint=None, params=None, headers=None, **param_kwargs):
+        params = params or {}
+        params = {**params, **param_kwargs}
+        _, _, _, cache_key = self.__get_defaults(method, endpoint, params, headers)
+        del self._cache[cache_key]
