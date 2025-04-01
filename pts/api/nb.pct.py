ceb2a8a92406330183531cc55d34615015903c0d
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-04-01 13:55:33 +0100

    refactor: moved over to nblite

---
+++ pts/api/nb.pct.py
@@ -0,0 +1,22 @@
+# %% [markdown]
+# # adulib.nb
+#
+# Utilities for working with notebooks.
+
+# %%
+#|default_exp nb
+
+# %%
+#| hide
+from nbdev.showdoc import show_doc
+
+# %%
+#| hide
+import nbdev; nbdev.nbdev_export()
+
+# %%
+#|export
+
+
+# %%
+import adulib.nb
