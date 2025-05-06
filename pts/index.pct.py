ded34ca01285ed11334b8cc3b59da1f3fe76fa08
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-05-06 13:46:28 +0100

    feat: updated readme

--- pts/index.pct.py
+++ pts/index.pct.py
@@ -28,15 +28,15 @@
 # uv sync --all-extras # Installs the virtual environment at './.venv'
 # direnv allow # Allows the automatic running of the script './.envrc'
 # nbl install-hooks # Installs some git hooks that ensures that notebooks are added properly
-# uv pip install -e .
 # ```
 #
 # You are now set up to develop the codebase.
 #
 # Further instructions:
 #
-# - To export notebooks run nbl export.
-# - To clean notebooks run nbl clean.
-# - To see other available commands run just nbl.
-# - To add a new dependency run uv add package-name. See the the uv documentation for more details.
-# - You need to git add all 'twinned' notebooks for the commit to be validated. For example, if you add nbs/my-nb.ipynb, you must also add pts/my-nb.pct.py.
+# - To export notebooks run `nbl export`.
+# - To clean notebooks run `nbl clean`.
+# - To see other available commands run just `nbl`.
+# - To add a new dependency run `uv add package-name`. See the the [uv documentation](https://docs.astral.sh/uv/) for more details.
+# - You need to git add all 'twinned' notebooks for the commit to be validated. For example, if you add `nbs/my-nb.ipynb`, you must also add `pts/my-nb.pct.py`.
+# - To render the documentation, run `nbl render-docs`. To preview it run `nbl preview-docs`
