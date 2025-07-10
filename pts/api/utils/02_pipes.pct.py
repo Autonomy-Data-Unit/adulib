f12203d655f926dc154dc38d81753c13cc4783c4
Lukas Kikuchi <lukas.kikuchi@gmail.com>
2025-07-10 22:43:13 +0100

    feat(utils.pipes): Added support for '>>' as well

--- pts/api/utils/02_pipes.pct.py
+++ pts/api/utils/02_pipes.pct.py
@@ -7,27 +7,41 @@
 # %%
 #|hide
 import nblite; from nblite import show_doc; nblite.nbl_export()
-import adulib.utils.pipes as this_module
 
 # %%
 #|export
 import pipe
-import tempfile
-import pickle
 
 
 # %%
 #|exporti
-class PipeRRShift:
+class Pipe:
     def __init__(self, func):
         self.__doc__ = func.__doc__
         self.func = func
 
     def __call__(self, *args, **kwargs):
-        return PipeRRShift(lambda x: self.func(x, *args, **kwargs))
+        return Pipe(lambda x: self.func(x, *args, **kwargs))
 
     def __rrshift__(self, other):
         return self.func(other)
+    
+    def __ror__(self, other):
+        return self.func(other)
+    
+class ConvertedPipe:
+    """Class to convert a `pipe.Pipe` to also be compatible with the `>>` operator."""
+    def __init__(self, old_pipe):
+        self.old_pipe = old_pipe
+
+    def __call__(self, *args, **kwargs):
+        return self.old_pipe(*args, **kwargs)
+
+    def __rrshift__(self, other):
+        return self.old_pipe.__ror__(other)
+    
+    def __ror__(self, other):
+        return self.old_pipe.__ror__(other)
 
 
 # %% [markdown]
@@ -35,15 +49,15 @@ class PipeRRShift:
 
 # %%
 #|export
-psum = pipe.Pipe(lambda x, start=0: sum(x, start=start))
-pmin = pipe.Pipe(lambda x, key=(lambda y:y): min(x, key=key))
-pmax = pipe.Pipe(lambda x, key=(lambda y:y): max(x, key=key))
-pabs = pipe.Pipe(lambda x: abs(x))
-padd = pipe.Pipe(lambda x, y: x + y)
-psub = pipe.Pipe(lambda x, y: x - y)
-pmul = pipe.Pipe(lambda x, y: x * y)
-pdiv = pipe.Pipe(lambda x, y: x / y)
-pmod = pipe.Pipe(lambda x, y: x % y)
+psum = Pipe(lambda x, start=0: sum(x, start=start))
+pmin = Pipe(lambda x, key=(lambda y:y): min(x, key=key))
+pmax = Pipe(lambda x, key=(lambda y:y): max(x, key=key))
+pabs = Pipe(lambda x: abs(x))
+padd = Pipe(lambda x, y: x + y)
+psub = Pipe(lambda x, y: x - y)
+pmul = Pipe(lambda x, y: x * y)
+pdiv = Pipe(lambda x, y: x / y)
+pmod = Pipe(lambda x, y: x % y)
 
 # %%
 [2,3,4] | psum(start=1) == sum([2,3,4], start=1)
@@ -69,15 +83,15 @@ pmod = pipe.Pipe(lambda x, y: x % y)
 
 # %%
 #|export
-ptype = pipe.Pipe(lambda x, typ: typ(x))
-plist = pipe.Pipe(lambda x: list(x))
-ptuple = pipe.Pipe(lambda x: tuple(x))
-pdict = pipe.Pipe(lambda x: dict(x))
-pset = pipe.Pipe(lambda x: set(x))
-pint = pipe.Pipe(lambda x: int(x))
-pfloat = pipe.Pipe(lambda x: float(x))
-pstr = pipe.Pipe(lambda x: str(x))
-pbool = pipe.Pipe(lambda x: bool(x))
+ptype = Pipe(lambda x, typ: typ(x))
+plist = Pipe(lambda x: list(x))
+ptuple = Pipe(lambda x: tuple(x))
+pdict = Pipe(lambda x: dict(x))
+pset = Pipe(lambda x: set(x))
+pint = Pipe(lambda x: int(x))
+pfloat = Pipe(lambda x: float(x))
+pstr = Pipe(lambda x: str(x))
+pbool = Pipe(lambda x: bool(x))
 
 # %%
 [1,6,2,6,2,4,6,7] | pset
@@ -87,13 +101,13 @@ pbool = pipe.Pipe(lambda x: bool(x))
 
 # %%
 #|export
-pjoin = pipe.Pipe(lambda x, sep: sep.join(str(i) for i in x))
-psplit = pipe.Pipe(lambda x, sep=None, maxsplit=-1: x.split(sep, maxsplit=maxsplit))
-pstrip = pipe.Pipe(lambda x, chars=None: x.strip(chars))
-plstrip = pipe.Pipe(lambda x, chars=None: x.lstrip(chars))
-prstrip = pipe.Pipe(lambda x, chars=None: x.rstrip(chars))
-pupper = pipe.Pipe(lambda x: x.upper())
-plower = pipe.Pipe(lambda x: x.lower())
+pjoin = Pipe(lambda x, sep: sep.join(str(i) for i in x))
+psplit = Pipe(lambda x, sep=None, maxsplit=-1: x.split(sep, maxsplit=maxsplit))
+pstrip = Pipe(lambda x, chars=None: x.strip(chars))
+plstrip = Pipe(lambda x, chars=None: x.lstrip(chars))
+prstrip = Pipe(lambda x, chars=None: x.rstrip(chars))
+pupper = Pipe(lambda x: x.upper())
+plower = Pipe(lambda x: x.lower())
 
 # %%
 "hello world!" | pupper | prstrip("!") | psplit | pjoin("    ")
@@ -103,10 +117,10 @@ plower = pipe.Pipe(lambda x: x.lower())
 
 # %%
 #|export
-pbind = pipe.Pipe(lambda x, f, *args, **kwargs: lambda *args2, **kwargs2: f(x, *args, *args2, **kwargs, **kwargs2))
-ppartial = pipe.Pipe(lambda f, *args, **kwargs: lambda *args2, **kwargs2: f(*args, *args2, **kwargs, **kwargs2))
-papply = pipe.Pipe(lambda x, f: f(x))
-pcall = pipe.Pipe(lambda f, *args, **kwargs: f(*args, **kwargs))
+pbind = Pipe(lambda x, f, *args, **kwargs: lambda *args2, **kwargs2: f(x, *args, *args2, **kwargs, **kwargs2))
+ppartial = Pipe(lambda f, *args, **kwargs: lambda *args2, **kwargs2: f(*args, *args2, **kwargs, **kwargs2))
+papply = Pipe(lambda x, f: f(x))
+pcall = Pipe(lambda f, *args, **kwargs: f(*args, **kwargs))
 
 
 # %%
@@ -138,33 +152,33 @@ def _pflatten_helper(lst, curr_level, levels=None):
         else:
             yield item
 
-pflatten = pipe.Pipe(lambda x, levels=None: _pflatten_helper(x, 0, levels))
-plen = pipe.Pipe(lambda x: len(x))
+pflatten = Pipe(lambda x, levels=None: _pflatten_helper(x, 0, levels))
+plen = Pipe(lambda x: len(x))
 
 # %%
 #|export
-pmap = pipe.map
-puniq = pipe.uniq
-ptranspose = pipe.transpose
-pfilter = pipe.filter
-psort = pipe.sort
-pt = pipe.t
-penumerate = pipe.enumerate
-pzip = pipe.izip
-pslice = pipe.islice
+pmap = ConvertedPipe(pipe.map)
+puniq = ConvertedPipe(pipe.uniq)
+ptranspose = ConvertedPipe(pipe.transpose)
+pfilter = ConvertedPipe(pipe.filter)
+psort = ConvertedPipe(pipe.sort)
+pt = ConvertedPipe(pipe.t)
+penumerate = ConvertedPipe(pipe.enumerate)
+pzip = ConvertedPipe(pipe.izip)
+pslice = ConvertedPipe(pipe.islice)
 
-pbatched = pipe.batched
-pchain = pipe.chain
-pchain_with = pipe.chain_with
-pdedup = pipe.dedup
-pgroupby = pipe.groupby
-ppermutations = pipe.permutations
-preverse = pipe.reverse
-pskip_while = pipe.skip_while
-ptail = pipe.tail
-ptake = pipe.take
-ptake_while = pipe.take_while
-ptraverse = pipe.traverse
+pbatched = ConvertedPipe(pipe.batched)
+pchain = ConvertedPipe(pipe.chain)
+pchain_with = ConvertedPipe(pipe.chain_with)
+pdedup = ConvertedPipe(pipe.dedup)
+pgroupby = ConvertedPipe(pipe.groupby)
+ppermutations = ConvertedPipe(pipe.permutations)
+preverse = ConvertedPipe(pipe.reverse)
+pskip_while = ConvertedPipe(pipe.skip_while)
+ptail = ConvertedPipe(pipe.tail)
+ptake = ConvertedPipe(pipe.take)
+ptake_while = ConvertedPipe(pipe.take_while)
+ptraverse = ConvertedPipe(pipe.traverse)
 
 # %%
 [1, [2, [3, [4, 5]]]] | pflatten | plist
@@ -180,9 +194,9 @@ ptraverse = pipe.traverse
 
 # %%
 #|export
-pget = pipe.Pipe(lambda x, k: x[k])
-pgetattr = pipe.Pipe(lambda x, k: getattr(x, k))
-ptee = pipe.tee
+pget = Pipe(lambda x, k: x[k])
+pgetattr = Pipe(lambda x, k: getattr(x, k))
+ptee = ConvertedPipe(pipe.tee)
 
 # %%
 {"key1": "value1", "key2": "value2"} | pget("key1")
@@ -201,22 +215,23 @@ Foo() | pgetattr("x")
 
 # %%
 #|export
-@pipe.Pipe
+@Pipe
 def pwrite(content, file_path=None, mode='w'):
+    import tempfile
     if file_path is None:
         file_path = tempfile.NamedTemporaryFile(delete=False).name
     with open(file_path, mode) as f:
         f.write(content)
     return file_path
     
-@pipe.Pipe
+@Pipe
 def pread(file_path, mode='r'):
     with open(file_path, mode) as f:
         return f.read()
     
-pshow = pipe.Pipe(lambda x: print(x) or x)  # Returns x for chaining
+pshow = Pipe(lambda x: print(x) or x)  # Returns x for chaining
 
-@pipe.Pipe
+@Pipe
 def pcopy(content):
     """Copy content to clipboard."""
     import pyperclip
@@ -236,18 +251,20 @@ def pcopy(content):
 
 # %%
 #|export
-@pipe.Pipe
+@Pipe
 def psave_pkl(obj, file_path=None):
     """Write an object to a pickle file."""
+    import pickle, tempfile
     if file_path is None:
         file_path = tempfile.NamedTemporaryFile(delete=False).name
     with open(file_path, 'wb') as f:
         pickle.dump(obj, f)
     return file_path
     
-@pipe.Pipe
+@Pipe
 def pload_pkl(file_path):
     """Read a pickle file and return the object."""
+    import pickle
     with open(file_path, 'rb') as f:
         return pickle.load(f)
 
@@ -258,13 +275,13 @@ def pload_pkl(file_path):
 
 # %%
 #|export
-@pipe.Pipe
+@Pipe
 def pto_df(data, **kwargs):
     """Create a pandas DataFrame."""
     import pandas as pd
     return pd.DataFrame(data, **kwargs)
 
-@PipeRRShift
+@Pipe
 def pcopy_df(df):
     """
     Copy a DataFrame to clipboard in a format compatible with Google Sheets or Excel.
@@ -278,7 +295,7 @@ def pcopy_df(df):
     df_to_clipboard(df)
     return df
 
-@PipeRRShift
+@Pipe
 def papply_mask(mask, df):
     """
     Apply a mask to a DataFrame.
@@ -310,13 +327,13 @@ df
 
 # %%
 #|export
-@pipe.Pipe
+@Pipe
 def pto_json(data, **kwargs):
     """Convert data to JSON string."""
     import json
     return json.dumps(data, **kwargs)
 
-@pipe.Pipe
+@Pipe
 def pfrom_json(json_str, **kwargs):
     import json
     """Convert JSON string to Python object."""
